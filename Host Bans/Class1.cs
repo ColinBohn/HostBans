9ff6db265400c534c3a6dcde2da158b2fa353e49
ColinBohn <colinbohn@gmail.com>
2013-10-20 11:44:23 -0700

    Have some files

---
+++ Host Bans/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Terraria;
+using TShockAPI;
+using TShockAPI.DB;
+using TerrariaApi.Server;
+using MySql.Data.MySqlClient;
+using Mono.Data.Sqlite;
+
+namespace HostBans
+{
+    [ApiVersion(1, 14)]
+    public class HostBans : TerrariaPlugin
+    {
+        public override Version Version
+        {
+            get { return new Version("1.1"); }
+        }
+        public override string Name
+        {
+            get { return "HostBans"; }
+        }
+        public override string Author
+        {
+            get { return "Colin"; }
+        }
+        public override string Description
+        {
+            get { return "Allow banning hostnames with reg ex"; }
+        }
+
+        public HostBans(Main game)
+            : base(game)
+        {
+            Order = 10;
+        }
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+            ServerApi.Hooks.ServerConnect.Register(this, OnConnect);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+                ServerApi.Hooks.ServerConnect.Deregister(this, OnConnect);
+            }
+            base.Dispose(disposing);
+        }
+        public void OnInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("hostban", HostBan, "hostban"));
+            SQL.SetupDB();
+        }
+        private void OnConnect(ConnectEventArgs args)
+        {
+            var player = new TSPlayer(args.Who);
+            System.Net.IPHostEntry host = System.Net.Dns.GetHostByAddress(player.IP);
+            SQL.HostBan ban = SQL.GetBanByHost(host.HostName);
+            if (ban != null)
+            {
+                    TShock.Utils.ForceKick(player, string.Format("Banned for: {0}.", ban.Reason), true, false);
+                    args.Handled = true;
+                    return;
+            }
+        }
+        public void HostBan(CommandArgs args)
+        {
+            if (args.Parameters.Count < 1)
+            {
+                args.Player.SendErrorMessage("Usage: /hostban [add|del] <input>");
+                args.Player.SendErrorMessage("/hostban add Username RegExHost Reason");
+                args.Player.SendErrorMessage("Usage: /hostban del Username");
+                return;
+            }
+            switch (args.Parameters[0])
+            {
+                case "add":
+                    {
+                        if (args.Parameters.Count != 4)
+                        {
+                            args.Player.SendErrorMessage("Invalid input!");
+                            return;
+                        }
+                        if (SQL.AddBan(args.Parameters[2], args.Parameters[1], args.Parameters[3], args.Player.Name, DateTime.Now))
+                        {
+                            args.Player.SendSuccessMessage("Ban has been added.");
+                        }
+                        else
+                        {
+                            args.Player.SendErrorMessage("ERROR: Adding the ban has failed.");
+                        }   
+                        break;
+                    }
+                case "del":
+                    {
+                        if (args.Parameters.Count != 2)
+                        {
+                            args.Player.SendErrorMessage("Invalid input!");
+                            return;
+                        }
+                        if (SQL.DeleteBanByUsername(args.Parameters[1]))
+                        {
+                            args.Player.SendSuccessMessage("Ban has been deleted.");
+                        }
+                        else
+                        {
+                            args.Player.SendErrorMessage("Ban could not be found.");
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        args.Player.SendErrorMessage("Invalid option!");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Host Bans/SQL.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Terraria;
+using TShockAPI;
+using TShockAPI.DB;
+using MySql.Data.MySqlClient;
+using Mono.Data.Sqlite;
+
+namespace HostBans
+{
+    public class SQL
+    {
+        public static SqlTableEditor SQLEditor;
+        public static SqlTableCreator SQLWriter;
+        private static IDbConnection db;
+
+        public static void SetupDB()
+        {
+            switch (TShock.Config.StorageType.ToLower())
+            {
+                case "mysql":
+                    string[] host = TShock.Config.MySqlHost.Split(':');
+                    db = new MySqlConnection()
+                    {
+                        ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                        host[0],
+                        host.Length == 1 ? "3306" : host[1],
+                        TShock.Config.MySqlDbName,
+                        TShock.Config.MySqlUsername,
+                        TShock.Config.MySqlPassword)
+                    };
+                    break;
+                case "sqlite":
+                    string sql = Path.Combine(TShock.SavePath, "signs.sqlite");
+                    db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+                    break;
+            }
+            SQLEditor = new SqlTableEditor(TShock.DB, TShock.DB.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+            SQLWriter = new SqlTableCreator(TShock.DB, TShock.DB.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+            var table = new SqlTable("hostbans",
+             new SqlColumn("ID", MySqlDbType.Int32) { Primary = true, AutoIncrement = true },
+             new SqlColumn("Host", MySqlDbType.Text),
+             new SqlColumn("Username", MySqlDbType.Text),
+             new SqlColumn("Reason", MySqlDbType.Text),
+             new SqlColumn("Admin", MySqlDbType.Text),
+             new SqlColumn("Date", MySqlDbType.Text)
+            );
+            SQLWriter.EnsureExists(table);
+        }
+        public static HostBan GetBanByHost(string host)
+        {
+            try
+            {
+                using (QueryResult reader = db.QueryReader("SELECT * FROM hostbans WHERE @0 REGEXP Host", host))
+                {
+                    if (reader.Read())
+                        return new HostBan(reader.Get<int>("ID"), reader.Get<string>("Host"), reader.Get<string>("Username"), reader.Get<string>("Reason"), reader.Get<string>("Admin"), Convert.ToDateTime(reader.Get<string>("Date")));
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            return null;
+        }
+        public static bool AddBan(string host, string username, string reason, string admin, DateTime date)
+        {
+            try
+            {
+                return db.Query("INSERT INTO hostbans (Host, Username, Reason, Admin, Date) VALUES (@0, @1, @2, @3, @4);", host, username, reason, admin, DateTime.UtcNow) != 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            return false;
+        }
+        public static bool DeleteBanByUsername(string name)
+        {
+            try
+            {
+                return db.Query("DELETE FROM hostbans where Username = @0", name) != 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            return false;
+        }
+        public class HostBan
+        {
+            public int ID { get; set; }
+            public string Host { get; set; }
+            public string Username { get; set; }
+            public string Reason { get; set; }
+            public string Admin { get; set; }
+            public DateTime Date { get; set; }
+            public HostBan(int id, string host, string username, string reason, string admin, DateTime date)
+            {
+                ID = id;
+                Host = host;
+                Username = username;
+                Reason = reason;
+                Admin = admin;
+                Date = date;
+            }
+            public HostBan()
+            {
+                ID = 0;
+                Host = "";
+                Username = "";
+                Reason = "";
+                Admin = "";
+                Date = DateTime.MinValue;
+            }
+
+        }
+    }
+}
